debea7aa3aacefe7829e83ce450111c523de73a4
KhushPatel2026 <khushspatel@gmail.com>
2025-07-04 22:34:51 +0530

    API chnage

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,3 @@
 export const SERVER_BASE_URL = 'https://api.petwellsolutions.com';
-export const SERVER_BASE_URL1 = 'https://petwell.onrender.com';
+export const SERVER_BASE_URL2 = 'https://petwell.onrender.com';
+export const SERVER_BASE_URL1 = 'http://localhost:3000';
